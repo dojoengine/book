bbb30ee7c2f05da4dd838bcd75c4deb1d93c4a5d
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-01-21 10:47:32 +1100

    build fix

--- dojo-book/docs/dist/assets/overview-oCI3OrOD.js
+++
@@ -1,9 +0,0 @@
-import{u as i,j as e}from"./index-m6NaWekR.js";const s={title:"Overview",description:"undefined"};function o(t){const n={a:"a",blockquote:"blockquote",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"overview",children:["Overview",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
-`,e.jsx(n.p,{children:"Dojo is BYO client, meaning that you can use any client you want to connect to the Dojo network."}),`
-`,e.jsxs(n.ul,{children:[`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/dojojs",children:"npm"})}),`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"torii.md",children:"torii"})}),`
-`]}),`
-`,e.jsxs(n.blockquote,{children:[`
-`,e.jsxs(n.p,{children:["Dojo is always looking to expand these clients, if you would like to contribute reach out into the ",e.jsx(n.a,{href:"https://discord.gg/KG9w9BmDrV",children:"Discord"})]}),`
-`]})]})}function c(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{c as default,s as frontmatter};
