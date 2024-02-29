15436f20799567446ad4e049ce98cf7e5b69158b
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-02-29 18:38:43 +1100

    add in icons, improve layouts

---
+++ docs/components/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <div>Released under the MIT License.</div>
+      <div>Copyright © 2022-present dojo</div>
+    </div>
+  );
+}
